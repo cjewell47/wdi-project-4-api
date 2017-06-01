bb8f43e75dab76fa062b3df3a0b1730b293834d5
cjewell47 <charles.jewell47@gmail.com>
2017-06-01 17:03:16 +0100

    token issue and decode

--- lib/auth.rb
+++ lib/auth.rb
@@ -3,14 +3,16 @@ require 'jwt'
 class Auth
   ALGORITHM = 'HS256'
 
-  def self.issue(payload, expiry_in_minutes=60*24*30)
-    payload[:exp] = expiry_in_minutes.minutes.from_now.to_i
+  def self.issue(payload, expiry=24.hours.from_now)
+    payload[:exp] = expiry.to_i
     JWT.encode(payload, auth_secret, ALGORITHM)
+ end
+
+  def self.decode(token, leeway=0)
+    decoded = JWT.decode(token, auth_secret, true, { leeway: leeway, algorithm: ALGORITHM })
+    HashWithIndifferentAccess.new(decoded[0])
   end
 
-  def self.decode
-  end
-  
   def self.auth_secret
     ENV["AUTH_SECRET"]
   end
