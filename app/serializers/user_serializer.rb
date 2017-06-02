0d2bcba41491f5b62f77dfc86f4532ad6ba3dce5
cjewell47 <charles.jewell47@gmail.com>
2017-06-02 17:13:30 +0100

    recipe table now exists

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
+  has_many :recipes
   attributes :id, :username, :email, :image, :password_digest
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :recipes
   scope :api do
     resources :users
     post 'login', to: 'authentications#login'
