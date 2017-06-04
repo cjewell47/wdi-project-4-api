7460031076117d5d6f388ca68e111273de2229aa
cjewell47 <charles.jewell47@gmail.com>
2017-06-04 17:16:27 +0100

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Ingredient.delete_all
 
 u1 = User.create!(username: "Charlie", email: "charlie@charlie.com", image: "https://www.stevensegallery.com/300/320", password: "password", password_confirmation: "password");
 u2 = User.create!(username: "Tiger", email: "tiger@woods.com", image: "http://www.ballerstatus.com/wp-content/uploads/2017/05/tiger-375x286.jpg", password: "password", password_confirmation: "password");
-u3 = User.create!(username: "Justin", email: "justin@beiber.com", image: "https://s-media-cache-ak0.pinimg.com/originals/6b/e2/3c/6be23cd9d4ed80dd9957a45984211133.jpg", password: "password", password_confirmation: "password");
+u3 = User.create!(username: "Justin", email: "justin@bieber.com", image: "https://s-media-cache-ak0.pinimg.com/originals/6b/e2/3c/6be23cd9d4ed80dd9957a45984211133.jpg", password: "password", password_confirmation: "password");
 u4 = User.create!(username: "Lindsay", email: "lindsay@lohan.com", image: "https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/124526.max-620x600.jpg", password: "password", password_confirmation: "password");
 
 
