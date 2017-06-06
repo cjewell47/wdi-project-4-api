9fa404c2b61991a7ed87009c3ce87ddaddf150c8
cjewell47 <charles.jewell47@gmail.com>
2017-06-06 13:50:12 +0100

    reseeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.delete_all
 Recipe.delete_all
 Ingredient.delete_all
 
-u1 = User.create!(username: "Charlie", email: "charlie@charlie.com", image: "https://www.stevensegallery.com/300/320", password: "password", password_confirmation: "password");
+u1 = User.create!(username: "Charlie", email: "charlie@charlie.com", image: "http://media.thedenverchannel.com/photo/2012/09/08/Charlie-Sheen-mugshot-22060079_112071_ver1.0_640_480.jpg", password: "password", password_confirmation: "password");
 u2 = User.create!(username: "Tiger", email: "tiger@woods.com", image: "http://www.ballerstatus.com/wp-content/uploads/2017/05/tiger-375x286.jpg", password: "password", password_confirmation: "password");
 u3 = User.create!(username: "Justin", email: "justin@bieber.com", image: "https://s-media-cache-ak0.pinimg.com/originals/6b/e2/3c/6be23cd9d4ed80dd9957a45984211133.jpg", password: "password", password_confirmation: "password");
 u4 = User.create!(username: "Lindsay", email: "lindsay@lohan.com", image: "https://d919ce141ef35c47fc40-b9166a60eccf0f83d2d9c63fa65b9129.ssl.cf5.rackcdn.com/images/124526.max-620x600.jpg", password: "password", password_confirmation: "password");
